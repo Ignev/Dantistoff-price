b4e10181b133596e8dd08b8bf98710ea7b1bc0e9
Zhenya Belov <belov.zhenya-belov1999@yandex.ru>
2022-11-29 20:50:02 +0300

    fix-mobile

--- js/script.js
+++ js/script.js
@@ -1,72 +1,101 @@
 window.addEventListener("DOMContentLoaded", function () {
-    const pricingAccordionContent = document.querySelectorAll(".pricing-accordion__content");
-    const pricingAccordionTitle = document.querySelector(".pricing-accordion__title");
-    const serveAccordionTitle = document.querySelectorAll(".serve-accordion__title")
-    const servesAccordionList = document.querySelectorAll(".serves-accordion__list")
-    console.log(serveAccordionTitle);
-    console.log(servesAccordionList);
+  const pricingAccordionContent = document.querySelector(".accordion__content"),
+    pricingAccordionTitle = document.querySelector(".pricing-accordion__title"),
+    serveAccordionTitle = document.querySelectorAll(".serve-accordion__title"),
+    servesAccordionList = document.querySelectorAll(".serves-accordion__list"),
+    collapsBtn = document.querySelector(".accordion-item__collaps"),
+    searchBtn = document.querySelector(".search__btn"),
+    searchInput = document.querySelector(".search__input"),
+    servesTitles = document.querySelectorAll(".serves-accordion-text__title"),
+    servesItem = document.querySelectorAll(".serves-accordion__item"),
+    searchValue = document.querySelector("#search__value"),
+    servesImg = document.querySelectorAll(".serves-accordion-info__img"),
+    tooltips = document.querySelectorAll(
+      ".serves-accordion-info__tooltip--mobile"
+    ),
+    serverClose = document.querySelectorAll(".serves-accordion-tooltip__close");
 
-    pricingAccordionTitle.addEventListener("click", function () {
-        pricingAccordionContent.forEach(content => {
-            if(content.classList.contains("accordion-disable")){
-                content.classList.remove("accordion-disable")
-                content.style.display = "flex"
-            }
-            else{
-                content.classList.add("accordion-disable")
-                setTimeout(() => {
-                    content.style.display = "none"
-                }, 400)
-                
-            }
-        })
-        if(pricingAccordionTitle.classList.contains("pricing-accordion__title-disabled")){
-            pricingAccordionTitle.classList.remove("pricing-accordion__title-disabled")
-        }
-        else{
-            pricingAccordionTitle.classList.add("pricing-accordion__title-disabled")
-        }
-    })
-    serveAccordionTitle[0].addEventListener("click", function () {
-        console.log(12);
-        if(servesAccordionList[0].classList.contains("accordion-disable")){
-            servesAccordionList[0].classList.remove("accordion-disable")
-            servesAccordionList[0].style.display = "block"
-        }
-        else{
-            servesAccordionList[0].classList.add("accordion-disable")
-            setTimeout(() => {
-                servesAccordionList[0].style.display = "none"
-            }, 400)
-            
-        }
-        if(serveAccordionTitle[0].classList.contains("serve-accordion__title-disable")){
-            serveAccordionTitle[0].classList.remove("serve-accordion__title-disable")
-        }
-        else{
-            serveAccordionTitle[0].classList.add("serve-accordion__title-disable")
-        }
-    })
-    serveAccordionTitle[2].addEventListener("click", function () {
-        console.log(12);
-        if(servesAccordionList[1].classList.contains("accordion-disable")){
-            servesAccordionList[1].classList.remove("accordion-disable")
-            servesAccordionList[1].style.display = "block"
-        }
-        else{
-            servesAccordionList[1].classList.add("accordion-disable")
-            setTimeout(() => {
-                servesAccordionList[1].style.display = "none"
-            }, 400)
-        }
-        if(serveAccordionTitle[2].classList.contains("serve-accordion-orange__title-disable")){
-            serveAccordionTitle[2].classList.remove("serve-accordion-orange__title-disable")
-        }
-        else{
-            serveAccordionTitle[2].classList.add("serve-accordion-orange__title-disable")
-        }
-    })
+  const openTooptop = (tooptip) => {
+    tooptip.classList.add("serves-accordion-info__tooltip--mobile--active");
+  };
+  const closeTooptop = (tooptip) => {
+    tooptip.classList.remove("serves-accordion-info__tooltip--mobile--active");
+  };
+  tooltips.forEach((tooltip) => {
+    servesImg.forEach((img) => {
+      img.addEventListener("click", () => {
+        console.log(+img.dataset.number);
+        openTooptop(tooltips[+img.dataset.number]);
+      });
+    });
+  });
+  tooltips.forEach((tooltip) => {
+    serverClose.forEach((close) => {
+        close.addEventListener("click", () => {
+        closeTooptop(tooltip);
+      });
+    });
+  });
 
+  //   const search = (input, listSearch, elementsSearch) => {
+  //     let value = input.value.trim();
+  //     if (value !== "") {
+  //       elementsSearch.forEach((element) => {
+  //         listSearch.forEach((listItem) => {
+  //           if (listItem.innerText.search(value) == -1) {
+  //             console.log(listItem.innerText);
+  //             element.classList.add("hide");
+  //           } else {
+  //             element.classList.remove("hide");
+  //           }
+  //         });
+  //       });
+  //     } else {
+  //       elementsSearch.forEach((element) => {
+  //         element.classList.remove("hide");
+  //       });
+  //     }
+  //     searchValue.innerText = value;
+  //   };
 
+  //   searchInput.addEventListener("input", () => {
+  //     search(searchInput, servesTitles, servesItem);
+  //   });
 
-})
+  const switchAccordion = (accordion) => {
+    if (accordion.classList.contains("accordion-disable")) {
+      accordion.classList.remove("accordion-disable");
+    } else {
+      accordion.classList.add("accordion-disable");
+    }
+  };
+
+  const switchArrow = (btn, selector) => {
+    if (btn.classList.contains(selector)) {
+      btn.classList.remove(selector);
+    } else {
+      btn.classList.add(selector);
+    }
+  };
+
+  collapsBtn.addEventListener("click", function () {
+    switchAccordion(pricingAccordionContent);
+    switchArrow(pricingAccordionTitle, "pricing-accordion__title-disabled");
+  });
+
+  pricingAccordionTitle.addEventListener("click", function () {
+    switchAccordion(pricingAccordionContent);
+    switchArrow(pricingAccordionTitle, "pricing-accordion__title-disabled");
+  });
+  serveAccordionTitle[0].addEventListener("click", function () {
+    switchAccordion(servesAccordionList[0]);
+    switchArrow(serveAccordionTitle[0], "serve-accordion__title-disable");
+  });
+  serveAccordionTitle[2].addEventListener("click", function () {
+    switchAccordion(servesAccordionList[1]);
+    switchArrow(
+      serveAccordionTitle[2],
+      "serve-accordion-orange__title-disable"
+    );
+  });
+});
